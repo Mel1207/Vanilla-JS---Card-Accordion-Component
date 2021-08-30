74a975d474d59c87397710ee1660aac4a6855f9d
Mel <meljohnpualon@gmail.com>
2021-08-30 20:41:23 +0800

    reversed class properties for open and close toggle

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 'use strict'
 
-const acbtn = document.querySelector('.accordion-btn');
+const acbtn = document.querySelectorAll('.accordion-btn');
 const acContent = document.querySelector('.accordion-content')
-
 
 const toggleOpen = function () {
     acContent.classList.toggle('open')
 }
 
-acbtn.addEventListener('click', toggleOpen)
+acbtn.forEach(btn => {
+    btn.addEventListener('click', toggleOpen)
+})
+
+
