d1d4bd391a8c18293c7fb4da03f6c10ffbc3d767
Mel <meljohnpualon@gmail.com>
2021-08-30 21:23:23 +0800

    fix targeted element and class toggle

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
 'use strict'
 
 const acbtn = document.querySelectorAll('.accordion-btn');
-const acContent = document.querySelector('.accordion-content')
+const acContent = document.querySelectorAll('.accordion-content');
+const accContainer = document.querySelector('.accordion-container');
 
-const toggleOpen = function () {
-    acContent.classList.toggle('open')
+const findClose = (e) => {
+    const target = e.target.closest('.accordion-btn')
+    
+
+    if(!target) return
+
+    acContent.forEach(cont => {
+        cont.classList.remove('open')
+    })
+
+    // target.firstElementChild.nextElementSibling
+    // console.log(target.firstElementChild())
+    console.log(target.dataset.src)
+    target.lastElementChild.classList.add('open')
+
+    // acContent.forEach(content => {
+    //     content.classList.remove('open')
+    // })
+
+    // if(target === true) {
+    //     document.querySelector(`.content${target.dataset.src}`).classList.add('open')
+    // }
 }
 
-acbtn.forEach(btn => {
-    btn.addEventListener('click', toggleOpen)
-})
-
+accContainer.addEventListener('click', findClose)
 
