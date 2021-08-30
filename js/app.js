3780bd6f09846bc6b27bf8e65d8edb0cd2769fcc
Mel <meljohnpualon@gmail.com>
2021-08-30 20:31:05 +0800

    fix class toggle style and function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const acbtn = document.querySelector('.accordion-btn');
+const acContent = document.querySelector('.accordion-content')
+
+
+const toggleOpen = function () {
+    acContent.classList.toggle('open')
+}
+
+acbtn.addEventListener('click', toggleOpen)
